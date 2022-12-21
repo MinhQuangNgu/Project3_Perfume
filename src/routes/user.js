08d37e23fbd1119ae814b8df095cd78d0f1a60eb
MinhQuangNgu <quangnm160393@gmail.com>
2022-12-21 10:21:40 +0700

    change

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,29 +1,69 @@
-const router = require('express').Router();
-const userController = require('../controllers/userController');
-const middleWareController = require('../controllers/middleWareController');
+const router = require("express").Router();
+const userController = require("../controllers/userController");
+const middleWareController = require("../controllers/middleWareController");
 
-router.post('/register',userController.register);
-router.post('/login',userController.login);
-router.get('/getall',middleWareController.verifyAdmin,userController.getUsers);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.get(
+    "/getall",
+    middleWareController.verifyAdmin,
+    userController.getUsers
+);
 
-router.get('/getone/:id',middleWareController.verifyUser,userController.adminGetOne);
+router.get(
+    "/getone/:id",
+    middleWareController.verifyUser,
+    userController.adminGetOne
+);
 
-router.delete('/delete/:id',middleWareController.verifyAdmin,userController.deleteUser);
+router.delete(
+    "/delete/:id",
+    middleWareController.verifyAdmin,
+    userController.deleteUser
+);
 
-router.post('/change_password',middleWareController.verifyUser,userController.changePassword);
+router.post(
+    "/change_password",
+    middleWareController.verifyUser,
+    userController.changePassword
+);
 
-router.put('/update/me',middleWareController.verifyUser,userController.addCart);
+router.put(
+    "/update/me",
+    middleWareController.verifyUser,
+    userController.addCart
+);
 
-router.put('/update',middleWareController.verifyUser,userController.updateUser);
+router.put(
+    "/update",
+    middleWareController.verifyUser,
+    userController.updateUser
+);
 
-router.post('/replace/:id',middleWareController.verifyUser,userController.replaceCart);
+router.post(
+    "/replace/:id",
+    middleWareController.verifyUser,
+    userController.replaceCart
+);
 
-router.post('/addcount/:id',middleWareController.verifyUser,userController.addCount);
+router.post(
+    "/addcount/:id",
+    middleWareController.verifyUser,
+    userController.addCount
+);
 
-router.post('/get_password',middleWareController.verifyAdmin,userController.getPassword);
+router.post(
+    "/get_password",
+    middleWareController.verifyAdmin,
+    userController.getPassword
+);
 
-router.post('/deleteall',middleWareController.verifyUser,userController.deleteAllCart);
+router.post(
+    "/deleteall",
+    middleWareController.verifyUser,
+    userController.deleteAllCart
+);
 
-router.post('/logout',userController.logout);
+router.post("/logout", userController.logout);
 
 module.exports = router;
