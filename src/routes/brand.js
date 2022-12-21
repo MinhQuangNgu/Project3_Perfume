411e7b15588d9e0aea7ecde9fc0e2ca5a578942a
MinhQuangNgu <quangnm160393@gmail.com>
2022-12-21 10:23:07 +0700

    change

--- src/routes/brand.js
+++ src/routes/brand.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
 const brandController = require("../controllers/brandController");
 const middleWareController = require("../controllers/middleWareController");
-router.delete("/delete/:slug", brandController.deleteBrand);
-router.put("/update/:slug", brandController.updateBrand);
-router.post("/create", brandController.createBrand);
+router.delete(
+    "/delete/:slug",
+    middleWareController.verifyAdmin,
+    brandController.deleteBrand
+);
+router.put(
+    "/update/:slug",
+    middleWareController.verifyAdmin,
+    brandController.updateBrand
+);
+router.post(
+    "/create",
+    middleWareController.verifyAdmin,
+    brandController.createBrand
+);
 router.get("/:categary", brandController.getBrandReply);
 router.get("/getone/:slug", brandController.getone);
 router.get("/", brandController.getBrand);
